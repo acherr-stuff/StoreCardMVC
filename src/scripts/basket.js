e5133a171fd4a4ab3982114bc36ef7c695dc5b90
Anastasia <Andreeva>
2021-08-02 15:35:56 +0300

    разделение логики для счетчика

--- src/scripts/basket.js
+++ src/scripts/basket.js
@@ -5,32 +5,52 @@ export class busketListModel {
     this.sumOfTheProducts = 0;
   }
 
+
   deleteFromBusket(id) {
 
     let delProduct = this.busketProducts.findIndex((x) => x.id == id);
-
-    // console.log(this.busketProducts[delProduct].name + "удален");
 
     this.sumOfTheProducts = this.sumOfTheProducts - this.busketProducts[delProduct].cost;
 
     this.busketProducts.splice(delProduct, 1);
 
-    // console.log("сумма покупок" + this.sumOfTheProducts);
   }
 
-  bindBusketListChanged(callback) {
-    this.onBusketListChanged = callback;
+  decreaseItemCount(item) {
+
+        let decProduct = this.busketProducts.findIndex((x) => x.id == item.id);
+
+        if ( this.busketProducts[decProduct].count>1) {
+           this.busketProducts[decProduct].count--;
+          this.busketProducts[decProduct].cost -=
+            this.busketProducts[decProduct].price;
+        }
+
+        // console.log(
+        //   this.busketProducts[decProduct].name +
+        //     " уменьшен, общая цена" +
+        //     this.busketProducts[decProduct].cost
+        // );
+
+
+      this.sumOfTheProducts = this.sumOfTheProducts - this.busketProducts[decProduct].price;
+      // console.log(" перерасчет цены" + this.sumOfTheProducts);
+     
+    }
   }
-}
+  
+
+  // bindBusketListChanged(callback) {
+  //   this.onBusketListChanged = callback;
+  // }
  
 export class busketListView {
   constructor() {
     this.busket = this.getElement("#busket_table");
     this.busketBottom = this.getElement("#busket_bottom");
     this.emptyBusket = this.getElement("#empty_busket");
-    
+
     // this.busket.className = "unactive";
-    
   }
 
   createElement(tag, className) {
@@ -41,6 +61,18 @@ export class busketListView {
   getElement(selector) {
     const element = document.querySelector(selector);
     return element;
+  }
+  //удаление строки корзины
+  removeRow(input) {
+    //Now we are getting the parent for the input element, marked as anyRow so we do not rely on fixed dom structure
+    //Furthermore we do not need to retrieve parentDiv anymore
+    var tP = (function getParent(e) {
+      return e.className.indexOf("busket__item") !== -1 || !e.parentNode
+        ? e
+        : getParent(e.parentNode);
+    })(input);
+
+    if (tP) tP.parentNode.removeChild(tP);
   }
 
   deleteItemFromBusketPage(id) {
@@ -53,24 +85,40 @@ export class busketListView {
 
     for (let i = 0; i < tL.length; i++) {
       if (tL[i].dataset.id == id) {
-        // this.model.sumOfTheProducts =
-        //   this.model.sumOfTheProducts - this.model.busketProducts[i].cost;
-        // this.view.busketBottom.innerText =
-        //   "ИТОГО: " + this.model.sumOfTheProducts + " Р";
-        // //удаление товара из массива элементов корзины
-        // this.model.busketProducts.splice(id - 1, 1);
-
-        //удалить div товара из корзины
-        removeRow(tL[i]);
+        this.removeRow(tL[i]);
       }
     }
-
     // console.log(this.model.busketProducts.length);
   }
- 
+
+  decreaseItemCount(item) {
+    let tCount = document.querySelectorAll(".busket__item__count__number");
+
+    for (let i = 0; i < tCount.length; i++) {
+      
+      if (tCount[i].dataset.id == item.id) {
+        console.log("новые значения будут отрисованы");
+
+        console.log("счетчик " + item.count);
+
+
+        document
+          .getElementById("busket_table")
+          .querySelector('[data-id="' + item.id + '"]')
+          .querySelector(".busket__item__count__number").innerText =
+          item.count;
+
+        document
+          .getElementById("busket_table")
+          .querySelector('[data-id="' + item.id + '"]')
+          .querySelector(".busket__item__cost").innerText =
+          item.cost;
+      }
+      }
+    }
+  
 
   displayNewItem(item) {
-
     let busketItem = this.createElement("div", "busket__item");
     busketItem.setAttribute("data-id", item.id);
     this.busket.appendChild(busketItem);
@@ -119,8 +167,6 @@ export class busketListView {
     busketItem.appendChild(busketItemDelete);
   }
 
-
-
   bindRemoveFromBusket(handler) {
     this.busket.addEventListener("click", (event) => {
       if (event.target.className === "busket__item__delete") {
@@ -150,6 +196,7 @@ export class busketListView {
   }
 }
 
+
 export class busketListController {
   constructor(model, view) {
     this.model = model;
@@ -161,8 +208,7 @@ export class busketListController {
     this.view.bindRemoveFromBusket(this.handleRemoveFromBusket.bind(this));
     this.view.bindIncreaseCount(this.handleIncreaseCount.bind(this));
     this.view.bindDecreaseCount(this.handleDecreaseCount.bind(this));
-    this.model.bindBusketListChanged(this.onBusketListChanged);
-
+    // this.model.bindBusketListChanged(this.onBusketListChanged);
 
     if (this.model.busketProducts.length == 0) {
       this.view.busket.classList.add("unactive");
@@ -170,67 +216,20 @@ export class busketListController {
   }
 
 
-  // deleteFromBusket(id) {
-  //   let tL = document.querySelectorAll(".busket__item");
-
-  //   if (tL.length == 1) {
-  //     this.view.busket.classList.add("unactive");
-  //     this.view.emptyBusket.classList.remove("unactive");
-  //   }
-
-  //   for (let i = 0; i < this.model.busketProducts.length; i++) {
-  //     if (tL[i].dataset.id == id) {
-  //       this.model.sumOfTheProducts =
-  //         this.model.sumOfTheProducts - this.model.busketProducts[i].cost;
-  //       this.view.busketBottom.innerText =
-  //         "ИТОГО: " + this.model.sumOfTheProducts + " Р";
-  //       //удаление товара из массива элементов корзины
-  //       this.model.busketProducts.splice(id - 1, 1);
-
-  //       //удалить div товара из корзины
-  //       removeRow(tL[i]);
-  //     }
-  //   }
-
-  //   console.log(this.model.busketProducts.length);
-  // }
-
   handleRemoveFromBusket(id) {
     this.model.deleteFromBusket(id);
     this.view.deleteItemFromBusketPage(id);
+    this.view.busketBottom.innerText = "ИТОГО: " + this.model.sumOfTheProducts + " Р";
+
   }
 
   handleDecreaseCount(id) {
-    let tCount = document.querySelectorAll(".busket__item__count__number");
+    
+    const item = this.model.busketProducts.find((x) => x.id == id);
+    this.model.decreaseItemCount(item);
+    this.view.decreaseItemCount(item);
+    this.view.busketBottom.innerText = "ИТОГО: " + this.model.sumOfTheProducts + " Р";
 
-    for (let i = 0; i < this.model.busketProducts.length; i++) {
-      if (
-        tCount[i].dataset.id == id &&
-        this.model.busketProducts[i].count > 1
-      ) {
-        this.model.busketProducts[i].count--;
-        this.model.busketProducts[i].cost -= this.model.busketProducts[i].price;
-
-        // console.log(this.model.busketProducts[i].cost);
-
-        document
-          .getElementById("busket_table")
-          .querySelector('[data-id="' + id + '"]')
-          .querySelector(".busket__item__count__number").innerText =
-          this.model.busketProducts[i].count;
-
-        document
-          .getElementById("busket_table")
-          .querySelector('[data-id="' + id + '"]')
-          .querySelector(".busket__item__cost").innerText =
-          this.model.busketProducts[i].cost;
-
-        this.model.sumOfTheProducts =
-          this.model.sumOfTheProducts - this.model.busketProducts[i].price;
-        this.view.busketBottom.innerText =
-          "ИТОГО: " + this.model.sumOfTheProducts + " Р";
-      }
-    }
   }
 
   handleIncreaseCount(id) {
@@ -310,31 +309,4 @@ export class busketListController {
     }
   }
 
-  // handleAddToBusket(id) {
-
-  //   if (this.model.busketProducts.length != 0) {
-  //     this.view.busket.classList.remove("unactive");
-  //     this.view.emptyBusket.classList.add("unactive");
-  //   }
-
-  //   this.addToBusket(id);
-
-  // }
 }
-
-
-
-
-function removeRow(input) {
-  //Now we are getting the parent for the input element, marked as anyRow so we do not rely on fixed dom structure
-  //Furthermore we do not need to retrieve parentDiv anymore
-  var tP = (function getParent(e) {
-    return e.className.indexOf("busket__item") !== -1 || !e.parentNode
-      ? e
-      : getParent(e.parentNode);
-  })(input);
-
-  if (tP) tP.parentNode.removeChild(tP);
-}
-
-
